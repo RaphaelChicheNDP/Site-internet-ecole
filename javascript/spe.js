5f8dac03c05e93c42f21af8598bc934ab6b59701
RaphaelChicheNDP <raphael.ch2004@gmail.com>
2022-02-20 13:01:36 +0100

    NDP site

---
+++ javascript/spe.js
@@ -0,0 +1,34 @@
+
+function svt(){  
+    document.getElementById("svt").style.background="linear-gradient(90deg, rgba(7,116,154,1) 24%, rgba(7,59,154,1) 52%, rgba(7,50,148,1) 63%, rgba(9,9,121,1) 84%, rgba(2,0,36,1) 100%, rgba(0,212,255,1) 100%)";
+}
+
+function svt2(){
+    document.getElementById("svt").style.background = "rgb(84, 93, 211)";
+}
+
+function bordurephy(){  
+    document.getElementById("physique-c").style.background="linear-gradient(90deg, rgba(7,116,154,1) 24%, rgba(7,59,154,1) 52%, rgba(7,50,148,1) 63%, rgba(9,9,121,1) 84%, rgba(2,0,36,1) 100%, rgba(0,212,255,1) 100%)";
+}
+
+function bordurephy2(){
+    document.getElementById("physique-c").style.background = "rgb(84, 93, 211)";
+}
+
+function bordureSI(){  
+    document.getElementById("si").style.background="linear-gradient(90deg, rgba(7,116,154,1) 24%, rgba(7,59,154,1) 52%, rgba(7,50,148,1) 63%, rgba(9,9,121,1) 84%, rgba(2,0,36,1) 100%, rgba(0,212,255,1) 100%)";
+}
+
+function bordureSI2(){
+    document.getElementById("si").style.background = "rgb(84, 93, 211)";
+}
+
+
+function bordureHLP(){  
+    document.getElementById("hlp").style.background="linear-gradient(90deg, rgba(7,116,154,1) 24%, rgba(7,59,154,1) 52%, rgba(7,50,148,1) 63%, rgba(9,9,121,1) 84%, rgba(2,0,36,1) 100%, rgba(0,212,255,1) 100%)";
+}
+
+function bordureHLP2(){
+    document.getElementById("hlp").style.background = "rgb(84, 93, 211)";
+}
+
